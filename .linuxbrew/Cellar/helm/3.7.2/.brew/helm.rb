c97d1275254d9502646327b4ba1788f682f7f7a9
Kerim <julian@creatorstraining.com>
2022-01-08 03:30:34 +0000

    commit

---
+++ .linuxbrew/Cellar/helm/3.7.2/.brew/helm.rb
@@ -0,0 +1,46 @@
+class Helm < Formula
+  desc "Kubernetes package manager"
+  homepage "https://helm.sh/"
+  url "https://github.com/helm/helm.git",
+      tag:      "v3.7.2",
+      revision: "663a896f4a815053445eec4153677ddc24a0a361"
+  license "Apache-2.0"
+  head "https://github.com/helm/helm.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    system "make", "build"
+    bin.install "bin/helm"
+
+    mkdir "man1" do
+      system bin/"helm", "docs", "--type", "man"
+      man1.install Dir["*"]
+    end
+
+    output = Utils.safe_popen_read(bin/"helm", "completion", "bash")
+    (bash_completion/"helm").write output
+
+    output = Utils.safe_popen_read(bin/"helm", "completion", "zsh")
+    (zsh_completion/"_helm").write output
+
+    output = Utils.safe_popen_read(bin/"helm", "completion", "fish")
+    (fish_completion/"helm.fish").write output
+  end
+
+  test do
+    system bin/"helm", "create", "foo"
+    assert File.directory? testpath/"foo/charts"
+
+    version_output = shell_output(bin/"helm version 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    if build.stable?
+      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
+      assert_match "GitCommit:\"#{revision}\"", version_output
+      assert_match "Version:\"v#{version}\"", version_output
+    end
+  end
+end
---
+++ .linuxbrew/Cellar/libmpc/1.2.1/.brew/libmpc.rb
@@ -0,0 +1,47 @@
+class Libmpc < Formula
+  desc "C library for the arithmetic of high precision complex numbers"
+  homepage "http://www.multiprecision.org/mpc/"
+  url "https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/mpc/mpc-1.2.1.tar.gz"
+  sha256 "17503d2c395dfcf106b622dc142683c1199431d095367c6aacba6eec30340459"
+  license "LGPL-3.0-or-later"
+
+  depends_on "gmp"
+  depends_on "mpfr"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --with-gmp=#{Formula["gmp"].opt_prefix}
+      --with-mpfr=#{Formula["mpfr"].opt_prefix}
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <mpc.h>
+      #include <assert.h>
+      #include <math.h>
+
+      int main() {
+        mpc_t x;
+        mpc_init2 (x, 256);
+        mpc_set_d_d (x, 1., INFINITY, MPC_RNDNN);
+        mpc_tanh (x, x, MPC_RNDNN);
+        assert (mpfr_nan_p (mpc_realref (x)) && mpfr_nan_p (mpc_imagref (x)));
+        mpc_clear (x);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-L#{Formula["mpfr"].opt_lib}",
+                   "-L#{Formula["gmp"].opt_lib}", "-lmpc", "-lmpfr",
+                   "-lgmp", "-o", "test"
+    system "./test"
+  end
+end
